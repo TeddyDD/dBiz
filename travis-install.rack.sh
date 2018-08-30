feb49d5eff610218bfc8ce4e9a7098b79b9b07cd
Daniel Lewan <vision360.daniel@gmail.com>
2018-08-30 23:34:50 +0200

    use TRAVIS_BUILD_DIR everywhere

--- travis-install.rack.sh
+++ travis-install.rack.sh
@@ -1,9 +1,10 @@
 #/bin/sh
 
 # clone and build rack
-git clone --recursive https://github.com/VCVRack/Rack.git
-cd Rack
+git clone --recursive https://github.com/VCVRack/Rack.git $TRAVIS_BUILD_DIR/Rack
+cd $TRAVIS_BUILD_DIR/Rack
 git checkout ${RACK_VER}
 make dep
 make
+cd $TRAVIS_BUILD_DIR
 
